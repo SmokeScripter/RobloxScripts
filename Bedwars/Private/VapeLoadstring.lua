0becf8597df81bed9f5e62d860b0d81f11c4b6d8
SmokeScripter <111685137+SmokeScripter@users.noreply.github.com>
2022-08-21 22:34:44 +0100

    Create VapeLoadstring.lua

---
+++ Bedwars/Private/VapeLoadstring.lua
@@ -0,0 +1,15 @@
+local Vape = {}
+local Whitelist = {game.Players.LocalPlayer.Name}
+
+function Vape.Module(Version, Is_Public)
+	if Is_Public == true then
+		return print("e")
+	elseif Is_Public == false then
+		return loadstring(game:HttpGet("https://raw.githubusercontent.com/SmokeScripter/RobloxScripts/main/Bedwars/Vape.lua", true))()
+	end
+	if Version == "V4" then
+		print("e")
+	end
+end
+
+Vape.Module('V4', false)
